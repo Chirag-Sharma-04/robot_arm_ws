878e1d793d0c0e8457fd7f3ce414edaf75f53a15
Chirag <chiragcs2004@gmail.com>
2024-11-02 14:59:48 +0530

    First

---
+++ build/robot_ctrl_py/share/robot_ctrl_py/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/ubantuu/robot_arm_ws/build/robot_ctrl_py"
---
+++ install/robot_description/share/robot_description/local_setup.sh
@@ -0,0 +1,1 @@
+/home/ubantuu/robot_arm_ws/build/robot_description/ament_cmake_environment_hooks/local_setup.sh
